dcfbd42d266fa35199970bb34289b4813072368a
DianaApz <kari_dia_12@hotmail.com>
2018-09-28 19:00:33 -0500

    act portada

--- src/index.js
+++ src/index.js
@@ -6,6 +6,26 @@
 //     fullWidth: true,
 //     indicators: false
 //  });
+$('.ml2').each(function(){
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+  });
+  
+  anime.timeline({loop: true})
+    .add({
+      targets: '.ml2 .letter',
+      opacity: [0,1],
+      easing: "easeInOutQuad",
+      duration: 2250,
+      delay: function(el, i) {
+        return 150 * (i+1)
+      }
+    }).add({
+      targets: '.ml2',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
 
 document.addEventListener('DOMContentLoaded', function() {
     M.AutoInit();
