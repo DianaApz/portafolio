50c4e806d5ed52f65b289dbedbdbebc1901fcc4d
DianaApz <kari_dia_12@hotmail.com>
2018-10-03 17:02:39 -0500

    mod px about me

--- src/index.js
+++ src/index.js
@@ -31,6 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
     M.AutoInit();
 });
 
+
 // document.addEventListener('DOMContentLoaded', function() {
 //     var elems = document.querySelectorAll('.slider');
 //     var instances = M.Slider.init(elems);
